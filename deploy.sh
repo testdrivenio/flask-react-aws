df558eb6769ebdeb6551093f975fd3296bf5f4ff
Michael Herman <hermanmu@gmail.com>
2019-07-28 21:35:24 -0600

    testing codebuild

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,7 @@ configure_aws_cli() {
   echo "AWS Configured!"
 }
 
+# new
 register_definition() {
   if revision=$(aws ecs register-task-definition --cli-input-json "$task_def" | $JQ '.taskDefinition.taskDefinitionArn'); then
     echo "Revision: $revision"
@@ -18,21 +19,32 @@ register_definition() {
   fi
 }
 
+update_service() {
+  if [[ $(aws ecs update-service --cluster $cluster --service $service --task-definition $revision | $JQ '.service.taskDefinition') != $revision ]]; then
+    echo "Error updating service."
+    return 1
+  fi
+}
+
 deploy_cluster() {
 
   # users
+  service="flask-react-users-service"  # new
   template="ecs_users_taskdefinition.json"
   task_template=$(cat "ecs/$template")
   task_def=$(printf "$task_template" $AWS_ACCOUNT_ID $AWS_RDS_URI $PRODUCTION_SECRET_KEY)
   echo "$task_def"
   register_definition
+  update_service  # update service
 
   # client
+  service="flask-react-client-service"  # new
   template="ecs_client_taskdefinition.json"
   task_template=$(cat "ecs/$template")
   task_def=$(printf "$task_template" $AWS_ACCOUNT_ID)
   echo "$task_def"
   register_definition
+  update_service  # update service
 
 }
 
