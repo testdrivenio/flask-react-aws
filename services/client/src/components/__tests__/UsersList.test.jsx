e1a19f2f7896acaa0aa00d85f6370aa40c77bb6f
Michael Herman <hermanmu@gmail.com>
2019-08-11 10:45:47 -0600

    heroku deploy

--- services/client/src/components/__tests__/UsersList.test.jsx
+++ services/client/src/components/__tests__/UsersList.test.jsx
@@ -22,12 +22,16 @@ const users = [
 ];
 
 it("renders a username", () => {
-  const { getByText } = render(<UsersList users={users} />);
+  const { getByText } = render(
+    <UsersList users={users} removeUser={() => true} />
+  );
   expect(getByText("michael")).toHaveClass("username");
   expect(getByText("michaelherman")).toHaveClass("username");
 });
 
 it("renders", () => {
-  const { asFragment } = render(<UsersList users={users} />);
+  const { asFragment } = render(
+    <UsersList users={users} removeUser={() => true} />
+  );
   expect(asFragment()).toMatchSnapshot();
 });
