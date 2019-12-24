ea0d2f06236bcc484d3e5040982cbab4154ff9f6
Michael Herman <hermanmu@gmail.com>
2019-12-24 09:04:57 -0700

    add react router

--- services/client/src/setupTests.js
+++ services/client/src/setupTests.js
@@ -1,5 +1,20 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
+/* global jest */
+
+import React from "react";
+import { Router } from "react-router-dom";
+import { render } from "@testing-library/react";
+import { createMemoryHistory } from "history";
 import "@testing-library/jest-dom/extend-expect";
+
+global.renderWithRouter = function renderWithRouter(
+  ui,
+  {
+    route = "/",
+    history = createMemoryHistory({ initialEntries: [route] })
+  } = {}
+) {
+  return {
+    ...render(<Router history={history}>{ui}</Router>),
+    history
+  };
+};
