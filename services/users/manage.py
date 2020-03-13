b5088a2371ed6cea2bbc349ee4f7e9715a3572f7
Michael Herman <hermanmu@gmail.com>
2020-03-12 20:45:51 -0400

    add auth routes

--- services/users/manage.py
+++ services/users/manage.py
@@ -13,19 +13,19 @@ app = create_app()
 cli = FlaskGroup(create_app=create_app)
 
 
-@cli.command('recreate_db')
+@cli.command("recreate_db")
 def recreate_db():
     db.drop_all()
     db.create_all()
     db.session.commit()
 
 
-@cli.command('seed_db')
+@cli.command("seed_db")
 def seed_db():
-    db.session.add(User(username='michael', email="hermanmu@gmail.com"))
-    db.session.add(User(username='michaelherman', email="michael@mherman.org"))
+    db.session.add(User(username="michael", email="hermanmu@gmail.com", password="supersecret"))
+    db.session.add(User(username="michaelherman", email="michael@mherman.org", password="supersecret"))
     db.session.commit()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli()
