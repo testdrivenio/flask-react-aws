6edf75a687cd659ae0d780ca231b107244550a11
Michael Herman <hermanmu@gmail.com>
2020-03-13 07:37:34 -0400

    add react bulma

---
+++ services/client/src/components/__tests__/NavBar.test.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { cleanup } from "@testing-library/react";
+
+import NavBar from "../NavBar";
+
+afterEach(cleanup);
+
+const title = "Hello, World!";
+
+it("renders a title", () => {
+  const { getByText } = renderWithRouter(<NavBar title={title} />);
+  expect(getByText(title)).toHaveClass("nav-title");
+});
+
+it("renders", () => {
+  const { asFragment } = renderWithRouter(<NavBar title={title} />);
+  expect(asFragment()).toMatchSnapshot();
+});
