e2e6ea76dd4e73fc7ac39fb9833245dbb175c154
Michael Herman <hermanmu@gmail.com>
2020-03-15 05:23:27 -0400

    refactor, add tests

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -17,16 +17,11 @@ class App extends Component {
       title: "TestDriven.io",
       accessToken: null
     };
-    this.addUser = this.addUser.bind(this);
-    this.handleRegisterFormSubmit = this.handleRegisterFormSubmit.bind(this);
-    this.handleLoginFormSubmit = this.handleLoginFormSubmit.bind(this);
-    this.isAuthenticated = this.isAuthenticated.bind(this);
-    this.logoutUser = this.logoutUser.bind(this);
   }
-  componentDidMount() {
+  componentDidMount = () => {
     this.getUsers();
-  }
-  getUsers() {
+  };
+  getUsers = () => {
     axios
       .get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
       .then(res => {
@@ -35,8 +30,8 @@ class App extends Component {
       .catch(err => {
         console.log(err);
       });
-  }
-  addUser(data) {
+  };
+  addUser = data => {
     axios
       .post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
       .then(res => {
@@ -46,8 +41,8 @@ class App extends Component {
       .catch(err => {
         console.log(err);
       });
-  }
-  handleRegisterFormSubmit(data) {
+  };
+  handleRegisterFormSubmit = data => {
     const url = `${process.env.REACT_APP_USERS_SERVICE_URL}/auth/register`;
     axios
       .post(url, data)
@@ -57,8 +52,8 @@ class App extends Component {
       .catch(err => {
         console.log(err);
       });
-  }
-  handleLoginFormSubmit(data) {
+  };
+  handleLoginFormSubmit = data => {
     const url = `${process.env.REACT_APP_USERS_SERVICE_URL}/auth/login`;
     axios
       .post(url, data)
@@ -70,14 +65,14 @@ class App extends Component {
       .catch(err => {
         console.log(err);
       });
-  }
-  isAuthenticated() {
+  };
+  isAuthenticated = () => {
     if (this.state.accessToken || this.validRefresh()) {
       return true;
     }
     return false;
-  }
-  validRefresh() {
+  };
+  validRefresh = () => {
     const token = window.localStorage.getItem("refreshToken");
     if (token) {
       axios
@@ -95,11 +90,11 @@ class App extends Component {
         });
     }
     return false;
-  }
-  logoutUser() {
+  };
+  logoutUser = () => {
     window.localStorage.removeItem("refreshToken");
     this.setState({ accessToken: null });
-  }
+  };
   render() {
     return (
       <div>
