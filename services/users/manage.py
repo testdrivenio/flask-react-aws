ef77c6aebc9dfa74e86e68c5010478f56778298f
Michael Herman <hermanmu@gmail.com>
2021-06-06 22:03:06 -0600

    lint

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,13 +1,5 @@
 # services/users/manage.py
 
-
-def test_passwords_are_random(test_app, test_database, add_user):
-    user_one = add_user('justatest', 'test@test.com', 'greaterthaneight')
-    user_two = add_user('justatest2', 'test@test2.com', 'greaterthaneight')
-    assert user_one.password != user_two.password
-
-import sys
-
 from flask.cli import FlaskGroup
 
 from src import create_app, db
